1e13ac2b5f1e92b92bde3ee4035ec156e4c65ac5
Nicholas Chamberlain <nicholaschamberlain@Macintosh-2.local>
2014-05-29 20:34:48 -0400

    More testing of next wednesday test

--- test/schedule.js
+++ test/schedule.js
@@ -3,16 +3,24 @@ var sinon = require('sinon');
 var chai = require('chai');
 var should = chai.should();
 
-describe('Moment manipulations', function() {
+describe('Moment manipulations', function () {
 
-	it('should instantiate moment of 05/27/14', function() {
+	it('should instantiate moment of 05/27/14', function () {
 		var date = moment('05-27-2014', 'MM-DD-YYYY');
 		date.format('MM-DD-YYYY').should.equal('05-27-2014');
 	});
 
-	it('should figure out what next Wednesdays date is', function() {
+	it('should figure out what next Wednesdays date is', function () {
 		var date = moment('05-27-2014', 'MM-DD-YYYY');
 		var nextWednesday = date.day(10);
-		nextWednesday.format('MM-DD-YYYY').should.equal('06-04-2014');
+		nextWednesday.weekday().should.equal(3);
+
+		date = moment('06-25-2014', 'MM-DD-YYYY');
+		nextWednesday = date.day(10);
+		nextWednesday.weekday().should.equal(3);
+
+		date = moment('06-15-2014', 'MM-DD-YYYY');
+		nextWednesday = date.day(10);
+		nextWednesday.weekday().should.equal(3);
 	});
 });

558a0ba12df64202ebce86de84e248cda8e5decf
Nicholas Chamberlain <nicholaschamberlain@Macintosh-2.local>
2014-05-31 13:07:09 -0400

    Refactored notificaiton tests and controller

--- test/notify.js
+++ test/notify.js
@@ -8,43 +8,51 @@ var async = require('async');
 var app = require('../app.js');
 var User = require('../models/User');
 
-describe('POST /testNotify', function() {
+describe('POST /testNotify', function () {
 
-	beforeEach(function() {
+	beforeEach(function (done) {
 		User.remove({}).exec();
+		return done();
 	});
 
-	it('duplicate user redirects to signup', function(done) {
+	it('should send 2 emails for 2 users', function (done) {
 
-		var user1 = new User({
+		var users = [];
+
+		users[0] = new User({
 			email: 'nick.chamberlain.jr@gmail.com',
 			name: 'test',
 			password: 'password'
 		});
 
-		var user2 = new User({
+		users[1] = new User({
 			email: 'nc38998@salisbury.edu',
 			name: 'test',
 			password: 'password'
 		});
 
-		user1.save(function(err) {
+		async.each(users, function (user, done) {
+			user.save(function (err) {
+				if (err) return done(err);
+				return done();
+			});
+		}, function (err) {
 			if (err) return done(err);
+			return done();
 		});
 
-		user2.save(function(err) {
-			if (err) return done(err);
-			// post notification button
-			request(app)
-				.post('/testNotify')
-				.end(function(err, res) {
-					if (err) return done(err);
-					return done();
-				});
-		});
+		request(app)
+			.post('/testNotify')
+			.end(function (err, res) {
+				if (err) return done(err);
+				// console.log(res.body.responses.length);
+				res.body.responses.length.should.equal(2);
+				return done();
+			});
 	});
 
-	afterEach(function() {
+	afterEach(function (done) {
 		User.remove({}).exec();
+		return done();
 	});
 });
